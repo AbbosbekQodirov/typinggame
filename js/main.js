e7e1f26882547a1e1d156e893fbd64c2a34664e0
Abbosbek <98879872+AbbosbekQodirov@users.noreply.github.com>
2023-02-07 11:50:04 +0500

    Update main.js
    security

--- js/main.js
+++ js/main.js
@@ -95,3 +95,17 @@ function hScore() {
   hightscore = num;
 }
 
+window.addEventListener("contextmenu", (event) => event.preventDefault());
+  window.document.onkeydown = function (e) {
+    if (
+      e.ctrlKey &&
+      (e.keyCode === 67 ,
+        e.keyCode === 86 ,
+        e.keyCode === 85 ||
+        e.keyCode === 117)
+    ) {
+      return false;
+    } else {
+      return true;
+    }
+  };
